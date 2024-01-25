be1fce8fc8f010e1b731e5a2ff06492cb91ca2a2
mitsuhir0 <cfx44093@gmail.com>
2024-01-25 14:46:38 +0000

    update

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import streamlit as st
 import oguid
 
 def convert(input: str, func) -> str:
+    """
+    テキストエリアからidのリストを受けとり、
+    引数にとった関数を適用して別のテキストエリアに返す
+    """
     lines = input.splitlines()
     result_lines = [func(line) for line in lines]
     result = "\n".join(result_lines)
     return result
 
+
 def gen_func(input: str, convert_to:str):
+    """
+    1行目の文字からidのタイプを推測し、ユーザーの指定するタイプに変換する関数を作る
+    inputには email, caddie, web serviceの3通り
+    outputも email, caddie, web serviceの3通り
+    """
     lines = input.splitlines()
     if lines == []:
         return passfunc
@@ -32,20 +42,22 @@ def gen_func(input: str, convert_to:str):
             result = passfunc
     return result
 
-st.title("OGU email-ID Converter ")
-col1, col2 = st.columns(2)
-
-email = email2 = "email"
-caddie = caddie2 = "OGU-Caddie"
-web = web2 = "OGU Web Service"
-
-with col1:
-    input = st.text_area("input (ctrl + enter)", height=465)
-    id_type = oguid.guess_id_type(input)
-
 
 def passfunc(input: str) -> str:
     return input
+
+
+st.title("OGU email-ID Converter ")
+col1, col2 = st.columns(2)
+
+email = "email"
+caddie = "OGU-Caddie"
+web = "OGU Web Service"
+
+with col1:
+    input = st.text_area("input", height=465)
+
+id_type = oguid.guess_id_type(input)
 
 if input is None:
     input = ""
@@ -54,11 +66,24 @@ with col2:
     if id_type is None:
         id_type = ""
     
-    l = [email, caddie, web]
+    match id_type:
+        case "email":
+            id_types = [caddie, web]
+        case "OGU-Caddie":
+            id_types = [email, web]
+        case "OGU Web Service":
+            id_types = [email, caddie]
+        case _:
+            id_types = [email, caddie, web]
+    
+    if id_type is "":
+        bold_id_type = ""
+    else:
+        bold_id_type = "**" + id_type + "**"
 
     convert_to = st.selectbox(
-        id_type + " to",
-        l
+        bold_id_type + " to",
+        id_types 
     )
     func = gen_func(input, convert_to)
     st.text_area("output", value=convert(input, func), height=380)
